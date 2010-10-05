5bbeb9ca65397a66f95c38d34a287639721e8d4e
sinsinpub <sinsinpub@25c6b6a4-e165-77e4-1d0b-f16a1be057c3>
2010-10-05 13:11:43 +0000

    * JRE_HOME added on startup priorly

--- UmdBrowser/UmdBrowser.bat
+++ UmdBrowser/UmdBrowser.bat
@@ -1,23 +1,31 @@
 @echo off
-set PATH=lib;lib\windows-x86\;%PATH%;
+setlocal
+set PATH=lib;lib\windows-x86;%PATH%;
 
-if "%JAVA_HOME%XXX"=="XXX" goto PFLOC
-if not exist "%JAVA_HOME%\jre\bin" goto JAVAMISSING
+if "%JRE_HOME%XXX"=="XXX" goto JAVAHOME
+if not exist "%JRE_HOME%\bin" goto JAVAMISSING
 echo Running UmdBrowser...
-"%JAVA_HOME%\jre\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
+"%JRE_HOME%\bin\java" -Xmx256m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
 goto END
 
-:PFLOC
+:JAVAHOME
+if "%JAVA_HOME%XXX"=="XXX" goto PROGFILE
+if not exist "%JAVA_HOME%\jre\bin" goto JAVAMISSING
+echo Running UmdBrowser...
+"%JAVA_HOME%\jre\bin\java" -Xmx256m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
+goto END
+
+:PROGFILE
 if "%programfiles(x86)%XXX"=="XXX" goto JAVA32
 if not exist "%programfiles(x86)%\Java\jre6\bin" goto JAVAMISSING
 echo Running UmdBrowser...
-"%programfiles(x86)%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
+"%programfiles(x86)%\Java\jre6\bin\java" -Xmx256m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
 goto END
 
 :JAVA32
 if not exist "%programfiles%\Java\jre6\bin" goto JAVAMISSING
 echo Running UmdBrowser...
-"%programfiles%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
+"%programfiles%\Java\jre6\bin\java" -Xmx256m -Djava.library.path=lib/windows-x86 -jar bin/umdbrowser.jar
 goto END
 
 :JAVAMISSING
@@ -26,3 +34,4 @@ echo Go to http://java.sun.com to install the 32bit Java JRE.
 pause
 
 :END
+endlocal
